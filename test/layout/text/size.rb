a7aa519e75cca3594d1b26c03e41ed10901a1fb5
Alexandre Barret <barret.alx@gmail.com>
2025-11-09 11:24:33 +1300

    Add more test on growing container with wrapped text

--- test/layout/text/size.rb
+++ test/layout/text/size.rb
@@ -21,7 +21,63 @@ class TestTextSize < MTest::Unit::TestCase
     ], commands)
   end
 
-  def test_wrapped_text
+  def test_wrapped_text_within_grow_container
+    layout = Panes.init(width: 100, height: 100)
+
+    commands = layout.build(id: 'root', width: Panes::Sizing.grow) do
+      text("Hello, World!", id: 'text', wrap: false)
+    end
+
+    assert_commands([
+      {
+        id: 'root',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 100, height: 1 },
+      },
+      {
+        id: 'text',
+        type: :text,
+        text: "Hello, World!",
+        bounding_box: { x: 0, y: 0, width: 13, height: 1 },
+      },
+    ], commands)
+  end
+
+  def test_wrapped_text_within_grow_container_and_max_width
+    layout = Panes.init(width: 100, height: 100)
+
+    commands = layout.build(id: 'root', width: Panes::Sizing.grow(max: 28)) do
+      text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor", id: 'text')
+    end
+
+    assert_commands([
+      {
+        id: 'root',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 28, height: 3 },
+      },
+      {
+        id: 'text',
+        type: :text,
+        text: "Lorem ipsum dolor sit amet,",
+        bounding_box: { x: 0, y: 0, width: 27, height: 1 },
+      },
+      {
+        id: 'text',
+        type: :text,
+        text: "consectetur adipisicing",
+        bounding_box: { x: 0, y: 1, width: 23, height: 1 },
+      },
+      {
+        id: 'text',
+        type: :text,
+        text: "elit, sed do eiusmod tempor",
+        bounding_box: { x: 0, y: 2, width: 27, height: 1 },
+      },
+    ], commands)
+  end
+
+  def test_wrapped_text_within_fit_container
     layout = Panes.init(width: 100, height: 100)
 
     commands = layout.build(id: 'root') do
