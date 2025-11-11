3a023b123d9a4a3c7115e9b87ab3d03c8ce34f1c
Alexandre Barret <barret.alx@gmail.com>
2025-11-11 21:19:23 +1300

    Draft :border renderer

--- mrblib/renderer.rb
+++ mrblib/renderer.rb
@@ -9,6 +9,22 @@ module Panes
         when :rectangle
         when :text
           tb.print(bbox[:x], bbox[:y], 0, 0, command[:text])
+        when :border
+          c = '+'.ord
+          x0 = (bbox[:x]).to_i
+          x1 = (bbox[:x]+bbox[:width]).to_i
+          y0 = (bbox[:y]).to_i
+          y1 = (bbox[:y]+bbox[:height]).to_i
+
+          (x0..x1).each { |x| tb.set_cell(x, y0, 0, 0, '-'.ord) }
+          (x0..x1).each { |x| tb.set_cell(x, y1, 0, 0, '-'.ord) }
+          (y0..y1).each { |y| tb.set_cell(x0, y, 0, 0, '|'.ord) }
+          (y0..y1).each { |y| tb.set_cell(x1, y, 0, 0, '|'.ord) }
+
+          tb.set_cell(x0, y0, 0, 0, c)
+          tb.set_cell(x1, y0, 0, 0, c)
+          tb.set_cell(x0, y1, 0, 0, c)
+          tb.set_cell(x1, y1, 0, 0, c)
         end
       end
     end
