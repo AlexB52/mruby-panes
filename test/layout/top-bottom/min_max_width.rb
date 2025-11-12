406ce46cc6d8c37d2f207c1913291bd7817a979f
Alexandre Barret <barret.alx@gmail.com>
2025-11-12 21:59:08 +1300

    create min max width test file

---
+++ test/layout/top-bottom/min_max_width.rb
@@ -0,0 +1,110 @@
+class TestWidthGrow < MTest::Unit::TestCase
+  def test_root_level_max_width_inferior_to_width_available
+    layout = Panes.init(height: 200, width: 200)
+
+    commands = layout.build(id: 'root', height: Panes::Sizing.grow(max: 50), width: 30, direction: :top_bottom)
+
+    assert_commands([
+      {
+        id: 'root',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 30, height: 50 },
+      },
+    ], commands)
+  end
+
+  def test_root_nested_levels_with_max_over_calculated_size
+    layout = Panes.init(height: 200, width: 200)
+
+    commands = layout.build(id: 'root', height: Panes::Sizing.grow, width: 30, direction: :top_bottom) do
+      ui(id: 'one',   height: Panes::Sizing.grow(max: 50),  width: 20)
+      ui(id: 'two',   height: Panes::Sizing.grow(max: 125), width: 20)
+      ui(id: 'third', height: Panes::Sizing.grow,           width: 20)
+    end
+
+    assert_commands([
+      {
+        id: 'root',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 30, height: 200 },
+      },
+      {
+        id: 'one',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 20, height: 50 },
+      },
+      {
+        id: 'two',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 50, width: 20, height: 75 },
+      },
+      {
+        id: 'third',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 125, width: 20, height: 75 },
+      },
+    ], commands)
+  end
+
+  def test_root_nested_levels_with_min_width_over_calculated_size
+    layout = Panes.init(height: 200, width: 200)
+
+    commands = layout.build(id: 'root', height: Panes::Sizing.grow, width: 30, direction: :top_bottom) do
+      ui(id: 'one',   height: Panes::Sizing.grow(max: 50),  width: 20)
+      ui(id: 'two',   height: Panes::Sizing.grow(min: 125), width: 20)
+      ui(id: 'third', height: Panes::Sizing.grow,           width: 20)
+    end
+
+    assert_commands([
+      {
+        id: 'root',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 30, height: 200 },
+      },
+      {
+        id: 'one',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 20, height: 37.5 },
+      },
+      {
+        id: 'two',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 37.5, width: 20, height: 125 },
+      },
+      {
+        id: 'third',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 162.5, width: 20, height: 37.5 },
+      },
+    ], commands)
+  end
+
+  def test_root_nested_levels_with_padding_and_child_gap
+    layout = Panes.init(height: 200, width: 200)
+
+    commands = layout.build(id: 'root', height: Panes::Sizing.grow, width: 30, padding: [5], child_gap: 5, direction: :top_bottom) do
+      ui(id: 'one',   height: Panes::Sizing.grow(max: 50),  width: 20)
+      ui(id: 'two',   height: Panes::Sizing.grow(min: 125), width: 20)
+    end
+
+    assert_commands([
+      {
+        id: 'root',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 30, height: 200 },
+      },
+      {
+        id: 'one',
+        type: :rectangle,
+        bounding_box: { x: 5, y: 5, width: 20, height: 50 },
+      },
+      {
+        id: 'two',
+        type: :rectangle,
+        bounding_box: { x: 5, y: 60, width: 20, height: 135 },
+      },
+    ], commands)
+  end
+end
+
+MTest::Unit.new.run
