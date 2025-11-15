8b1b40b5bc07877a7ed2b786bec0b75af6a9cbaf
Alexandre Barret <AlexB52@users.noreply.github.com>
2025-11-15 19:24:09 +1300

    Add coverage for normalized text alignment

--- test/layout/text/alignment.rb
+++ test/layout/text/alignment.rb
@@ -59,6 +59,32 @@ class TestTextAlignment < MTest::Unit::TestCase
     ], commands)
   end
 
+  def test_string_alignment_is_normalized
+    layout = Panes.init(width: 10, height: 3)
+
+    commands = layout.build(id: 'root', width: Panes::Sizing.grow) do
+      text('Hello', id: 'text', wrap: false, align: 'right')
+    end
+
+    assert_commands([
+      {
+        id: 'root',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 10, height: 1 },
+        bg_color: 0,
+        fg_color: 0,
+      },
+      {
+        id: 'text',
+        type: :text,
+        text: 'Hello',
+        bounding_box: { x: 5, y: 0, width: 5, height: 1 },
+        bg_color: 0,
+        fg_color: 0,
+      },
+    ], commands)
+  end
+
   def test_inline_text_honors_alignment
     layout = Panes.init(width: 20, height: 5)
 
@@ -90,6 +116,43 @@ class TestTextAlignment < MTest::Unit::TestCase
         type: :text,
         text: 'Alex',
         bounding_box: { x: 16, y: 0, width: 4, height: 1 },
+        bg_color: 0,
+        fg_color: 0,
+      },
+    ], commands)
+  end
+
+  def test_inline_center_alignment_divides_remaining_space
+    layout = Panes.init(width: 20, height: 5)
+
+    commands = layout.build(id: 'root', width: Panes::Sizing.grow) do
+      text(id: 'text', wrap: false, align: :center) do
+        text('Score:')
+        text(' 42')
+      end
+    end
+
+    assert_commands([
+      {
+        id: 'root',
+        type: :rectangle,
+        bounding_box: { x: 0, y: 0, width: 20, height: 1 },
+        bg_color: 0,
+        fg_color: 0,
+      },
+      {
+        id: 'text',
+        type: :text,
+        text: 'Score:',
+        bounding_box: { x: 5, y: 0, width: 6, height: 1 },
+        bg_color: 0,
+        fg_color: 0,
+      },
+      {
+        id: 'text',
+        type: :text,
+        text: ' 42',
+        bounding_box: { x: 11, y: 0, width: 3, height: 1 },
         bg_color: 0,
         fg_color: 0,
       },
