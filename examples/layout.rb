735db39267f9b3c6397e11da4913f35c9dc98405
Alexandre Barret <barret.alx@gmail.com>
2025-11-15 19:53:05 +1300

    fixup! Fix length printed by renderer

--- examples/layout.rb
+++ examples/layout.rb
@@ -14,15 +14,15 @@ begin
 
     commands = layout.build(id: 'root', width: Panes::Sizing.grow, height: Panes::Sizing.grow, border: { all: [:black, :default] }, direction: :top_bottom) do
       ui(id: '1st', width: Panes::Sizing.grow, height: Panes::Sizing.grow, bg_color: :red) do
-        ui(width: Panes::Sizing.grow, height: Panes::Sizing.grow, border: { all: [:black, :default] }) do
+        ui(width: Panes::Sizing.grow, height: Panes::Sizing.grow, fg_color: :white, border: { all: [:black, :default] }) do
           text(<<~TEXT)
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           TEXT
         end
-        ui(width: Panes::Sizing.grow, height: Panes::Sizing.grow, border: { all: [:black, :default] }) do
+        ui(width: Panes::Sizing.grow, height: Panes::Sizing.grow, fg_color: :white, border: { all: [:black, :default] }) do
           text("Duration: #{duration}")
         end
-        ui(width: Panes::Sizing.grow, height: Panes::Sizing.grow, border: { all: [:black, :default] }) do
+        ui(width: Panes::Sizing.grow, height: Panes::Sizing.grow, fg_color: :white, border: { all: [:black, :default] }) do
           text("Hello, World!")
         end
       end
