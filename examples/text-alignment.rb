ee263f77b74ba85b2409e9d505f275494f3130e9
Alexandre Barret <barret.alx@gmail.com>
2025-11-15 21:03:45 +1300

    update the example file

--- examples/text-alignment.rb
+++ examples/text-alignment.rb
@@ -7,17 +7,15 @@ begin
     layout.width = Termbox2.width
     layout.height = Termbox2.height
 
-    commands = layout.build(id: 'root', width: Panes::Sizing.grow, height: Panes::Sizing.grow, direction: :top_bottom) do
-      ui(width: Panes::Sizing.grow, height: Panes::Sizing.grow, border: { all: [:black, :default] }, padding: [1], child_gap: 1) do
+    commands = layout.build(id: 'root', width: Panes::Sizing.grow, height: Panes::Sizing.grow) do
+      ui(width: Panes::Sizing.grow, height: 10, border: {all: [:default, :default]}) do
         text('Left aligned text stays flush with the container edge.', align: :left)
+      end
+      ui(width: Panes::Sizing.grow, height: 10, border: {all: [:default, :default]}) do
         text('Centered text distributes space evenly and wraps within the box to stay centered.', align: :center)
+      end
+      ui(width: Panes::Sizing.grow, height: 10, border: {all: [:default, :default]}) do
         text('Right aligned text keeps the end of each line tight to the boundary.', align: :right)
-
-        text('Inline segments share the same alignment:') do
-          text(' first ', bg_color: :blue, fg_color: :white)
-          text('second ', bg_color: :green, fg_color: :black)
-          text('third', bg_color: :yellow, fg_color: :black)
-        end
       end
     end
 
